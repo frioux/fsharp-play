815c10970ecbd6f916780c558eb9b0967ea824d0
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2014-03-20 18:50:41 -0500

    example of use in action

---
+++ gc1.fs
@@ -0,0 +1,15 @@
+let createDisposable name =
+  printfn "creating: %s" name
+  { new System.IDisposable with
+    member x.Dispose() =
+      printfn "disposing: %s" name
+  }
+
+let testDisposable() =
+  use root = createDisposable "outer"
+  for i in [1..2] do
+    use nested = createDisposable (sprintf "inner %i" i)
+    printfn "completing iteration %i" i
+  printfn "leaving function"
+
+testDisposable ()
